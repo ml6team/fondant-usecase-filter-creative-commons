3e119ec23b41eb7fddc7af8798c862a909c6b533
Georges Lorre <georges.lorre@ml6.eu>
2023-12-07 15:47:21 +0100

    Implement new pipeline interface

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,46 +1,53 @@
-# Pipeline code goes here
-import logging
-import sys
+"""Pipeline to download and filter images from the Common Crawl dataset.""" ""
+from pathlib import Path
 
-from fondant.pipeline import ComponentOp, Pipeline
-
-
-logger = logging.getLogger(__name__)
-sys.path.append("../")
+import pyarrow as pa
+from fondant.pipeline import Pipeline
 
 PIPELINE_NAME = "cc-image-filter-pipeline"
 PIPELINE_DESCRIPTION = "Load cc image dataset"
-BASE_PATH = "./data-dir"
+BASE_PATH = "./fondant-artifacts"
+
+# Create data directory if it doesn't exist
+Path(BASE_PATH).mkdir(parents=True, exist_ok=True)
 
 # Define pipeline
-pipeline = Pipeline(pipeline_name=PIPELINE_NAME, base_path=BASE_PATH)
+pipeline = Pipeline(
+    name=PIPELINE_NAME, description=PIPELINE_DESCRIPTION, base_path=BASE_PATH
+)
 
 # Load from hub component
-load_component_column_mapping = {
-    "alt_text": "images_alt+text",
-    "image_url": "images_url",
-    "license_location": "images_license+location",
-    "license_type": "images_license+type",
-    "webpage_url": "images_webpage+url",
-    "surt_url": "images_surt+url",
-    "top_level_domain": "images_top+level+domain",
-}
-
-load_from_hf_hub = ComponentOp(
-    component_dir="components/load_from_hf_hub",
+raw_data = pipeline.read(
+    "load_from_hf_hub",
     arguments={
         "dataset_name": "fondant-ai/fondant-cc-25m",
-        "column_name_mapping": load_component_column_mapping,
         "n_rows_to_load": 100,  # Modify the number of images you want to download.
+    },
+    produces={
+        "alt_text": pa.string(),
+        "image_url": pa.string(),
+        "license_location": pa.string(),
+        "license_type": pa.string(),
+        "webpage_url": pa.string(),
+        "surt_url": pa.string(),
+        "top_level_domain": pa.string(),
     },
 )
 
 # Download images component
-download_images = ComponentOp.from_registry(
-    name="download_images",
-    arguments={"input_partition_rows": 100, "resize_mode": "no"},
+images = raw_data.apply(
+    "download_images",
+    arguments={
+        "input_partition_rows": 100,
+        "resize_mode": "no",
+    },
 )
 
-# Add components to the pipeline
-pipeline.add_op(load_from_hf_hub)
-pipeline.add_op(download_images, dependencies=[load_from_hf_hub])
+# Filter images component
+big_images = images.apply(
+    "filter_image_resolution",
+    arguments={
+        "min_image_dim": 512,
+        "max_aspect_ratio": 2.5,
+    },
+)
